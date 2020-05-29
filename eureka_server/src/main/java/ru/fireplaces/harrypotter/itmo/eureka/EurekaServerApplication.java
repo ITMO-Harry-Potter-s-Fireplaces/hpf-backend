20deb159dd1acd30b295fb3d5869957427b5478a
seniorkot <seniorkot24@gmail.com>
2020-05-29 07:53:01 +0300

    Refactor Proxy and Eureka packages

--- eureka_server/src/main/java/ru/fireplaces/harrypotter/itmo/eureka/EurekaServerApplication.java
+++ eureka_server/src/main/java/ru/fireplaces/harrypotter/itmo/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package ru.fireplaces.harrypotter.itmo;
+package ru.fireplaces.harrypotter.itmo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
