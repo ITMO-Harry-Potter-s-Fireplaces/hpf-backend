331f22b7be73de75eda21bc33efac15760ca43b4
seniorkot <seniorkot24@gmail.com>
2020-12-17 12:57:33 +0700

    Add new credentials to data.sql

--- security_service/src/main/resources/data.sql
+++ security_service/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into users (name, surname, middle_name, email, password, role, date_birth, active)
 values ('admin', 'admin', 'admin', 'admin@admin.com', '202cb962ac59075b964b07152d234b70', 0, '2020-01-01', true);
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('moder', 'moder', 'moder', 'moder@moder.com', '202cb962ac59075b964b07152d234b70', 1, '2020-01-01', true);
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('minister', 'minister', 'minister', 'minister@minister.com', '202cb962ac59075b964b07152d234b70', 2, '2020-01-01', true);
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('user', 'user', 'user', 'user@user.com', '202cb962ac59075b964b07152d234b70', 3, '2020-01-01', true);
