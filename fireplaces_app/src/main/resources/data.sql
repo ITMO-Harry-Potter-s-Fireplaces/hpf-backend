7348e87848f191df0690680cc6847d9becd8787e
seniorkot <seniorkot24@gmail.com>
2021-01-25 05:58:39 +0700

    Add monolith module for further migration

---
+++ fireplaces_app/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('admin', 'admin', 'admin', 'admin@admin.com', '202cb962ac59075b964b07152d234b70', 0, '2020-01-01', true);
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('moder', 'moder', 'moder', 'moder@moder.com', '202cb962ac59075b964b07152d234b70', 1, '2020-01-01', true);
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('minister', 'minister', 'minister', 'minister@minister.com', '202cb962ac59075b964b07152d234b70', 2, '2020-01-01', true);
+insert into users (name, surname, middle_name, email, password, role, date_birth, active)
+values ('user', 'user', 'user', 'user@user.com', '202cb962ac59075b964b07152d234b70', 3, '2020-01-01', true);
+
+
+insert into fireplaces (lat, lng, description) values (59.8631629507745, 30.3195093199611, 'test');
+insert into fireplaces (lat, lng, description) values (59.9191264465338, 30.2953982725739, 'test');
+insert into fireplaces (lat, lng, description) values (59.9266642119421, 30.3459401428699, 'test');
+insert into fireplaces (lat, lng, description) values (59.9363784468659, 30.3182808682323, 'test');
+insert into fireplaces (lat, lng, description) values (59.9428731744406, 30.3469670936465, 'test');
---
+++ fireplaces_app/src/main/resources/schema.sql
@@ -0,0 +1,35 @@
+-- Create sequences --
+CREATE SEQUENCE users_id_seq MINVALUE 0 START 0 INCREMENT 1;
+CREATE SEQUENCE fireplaces_id_seq MINVALUE 0 START 0 INCREMENT 1;
+CREATE SEQUENCE claims_id_seq MINVALUE 0 START 0 INCREMENT 1;
+
+-- Create tables --
+CREATE TABLE users(
+    id integer DEFAULT nextval('public.users_id_seq'::regclass) CONSTRAINT users_pkey PRIMARY KEY,
+    email varchar(127) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL,
+    role integer NOT NULL,
+    name varchar(63),
+    surname varchar(63),
+    middle_name varchar(63),
+    date_birth date,
+    active boolean
+);
+
+CREATE TABLE fireplaces(
+    id integer DEFAULT nextval('public.fireplaces_id_seq'::regclass) CONSTRAINT fireplaces_pkey PRIMARY KEY,
+    lat float NOT NULL,
+    lng float NOT NULL,
+    description varchar(511)
+);
+
+CREATE TABLE claims(
+    id integer DEFAULT nextval('public.claims_id_seq'::regclass) CONSTRAINT claims_pkey PRIMARY KEY,
+    status integer NOT NULL,
+    departure_id integer NOT NULL CONSTRAINT dep_claim_fireplace_fk REFERENCES fireplaces(id),
+    arrival_id integer NOT NULL CONSTRAINT arr_claim_fireplace_fk REFERENCES fireplaces(id),
+    departure_time timestamp NOT NULL,
+    user_id integer NOT NULL,
+    created timestamp NOT NULL,
+    modified timestamp NOT NULL
+);
