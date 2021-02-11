4fcf493de096960322e33ee76d4f81f252f652f5
seniorkot <seniorkot24@gmail.com>
2021-02-11 17:20:57 +0300

    Add some fireplaces to data.sql

--- fireplaces_app/src/main/resources/data.sql
+++ fireplaces_app/src/main/resources/data.sql
@@ -7,8 +7,24 @@ values ('minister', 'minister', 'minister', 'minister@minister.com', '202cb962ac
 insert into users (name, surname, middle_name, email, password, role, date_birth, active)
 values ('user', 'user', 'user', 'user@user.com', '202cb962ac59075b964b07152d234b70', 3, '2020-01-01', true);
 
-insert into fireplaces (lat, lng, description) values (59.8631629507745, 30.3195093199611, 'test');
-insert into fireplaces (lat, lng, description) values (59.9191264465338, 30.2953982725739, 'test');
-insert into fireplaces (lat, lng, description) values (59.9266642119421, 30.3459401428699, 'test');
-insert into fireplaces (lat, lng, description) values (59.9363784468659, 30.3182808682323, 'test');
-insert into fireplaces (lat, lng, description) values (59.9428731744406, 30.3469670936465, 'test');
+insert into fireplaces (lat, lng, description) values (59.94000900379223, 30.314988868335657, 'Эрмитаж');
+insert into fireplaces (lat, lng, description) values (59.941776451379894, 30.30512950548935, 'Кунсткамера');
+insert into fireplaces (lat, lng, description) values (59.93419348706508, 30.3063427424606, 'Исаакиевский Собор');
+insert into fireplaces (lat, lng, description) values (59.94029653059638, 30.329061804831543, 'Спас на Крови');
+insert into fireplaces (lat, lng, description) values (59.938930754968794, 30.332344471997587, 'Русский музей');
+insert into fireplaces (lat, lng, description) values (59.94401119134, 30.33190649371704, 'Марсово поле');
+insert into fireplaces (lat, lng, description) values (59.92668930212732, 30.29565269860977, 'Мариинский театр');
+insert into fireplaces (lat, lng, description) values (59.95170508370235, 30.319511383574433, 'Петропавловская крепость');
+insert into fireplaces (lat, lng, description) values (59.9573242156731, 30.309120060425066, 'ИТМО Главный корпус');
+insert into fireplaces (lat, lng, description) values (59.92696044047841, 30.33798763652866, 'ИТМО Ломоносова 9');
+insert into fireplaces (lat, lng, description) values (59.929500466803326, 30.31553045906661, 'ИТМО На Гривцова');
+insert into fireplaces (lat, lng, description) values (59.94483390751547, 30.294768143754776, 'ИТМО Биржевая линия');
+insert into fireplaces (lat, lng, description) values (59.972962131276425, 30.220685168646757, 'Газпром Арена');
+insert into fireplaces (lat, lng, description) values (59.95290843340878, 30.28185932213974, 'Стадион Петровский');
+insert into fireplaces (lat, lng, description) values (59.98835429582182, 30.17771425186338, 'Лахтацентр');
+insert into fireplaces (lat, lng, description) values (59.79984837623902, 30.271514784632146, 'Аэропорт Пулково');
+insert into fireplaces (lat, lng, description) values (59.92958849318836, 30.362226353607955, 'Московский вокзал');
+insert into fireplaces (lat, lng, description) values (59.90680698377783, 30.29866179810812, 'Балтийский вокзал');
+insert into fireplaces (lat, lng, description) values (59.86918005390509, 30.308202935732528, 'МСГ');
+insert into fireplaces (lat, lng, description) values (59.84834046527625, 30.32954517336017, 'Ленсовета 23 (общежитие)');
+insert into fireplaces (lat, lng, description) values (59.97257720166168, 30.30249199198259, 'Вязьма (общежитие)');
